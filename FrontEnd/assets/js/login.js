0c417bf836651c6da286b9104729818cc8de9eff
Kayyro <robin.estebe8@epitech.eu>
2026-03-25 17:07:04 +0100

    Creation de "login.js"  appel des differents elements du html

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,5 @@
+const btnLogin = document.querySelector("#btn-login");
+const inputEmail = document.querySelector("#email");
+const inputPassword = document.querySelector("#password");
+const errorMessage = document.querySelector("#error-message");
+
