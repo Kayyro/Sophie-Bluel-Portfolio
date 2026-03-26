5bad53b8bda1cf025393c58911643d78f4b90e82
Kayyro <robin.estebe8@epitech.eu>
2026-03-26 16:58:58 +0100

    finalisation du login.js avec gestion du token et redirection

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -4,20 +4,28 @@ const inputPassword = document.querySelector("#password");
 const errorMessage = document.querySelector("#error-message");
 
 btnLogin.addEventListener("click", async () => {
+  const email = inputEmail.value;
+  const password = inputPassword.value;
 
-    const email = inputEmail.value;
-    const password = inputPassword.value;
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
+  if (response.ok) {
+    const data = await response.json();
 
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    });
+    localStorage.setItem("token", data.token);
 
-    console.log(response);
+    //redirige vers l'accueil
+    window.location.href = "index.html";
+  } else {
+    //affiche l'erreur
+    errorMessage.textContent = "Identifiants incorrects, veuillez réessayer.";
+  }
 });
