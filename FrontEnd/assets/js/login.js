b8ee44d97878cd8776d9df5e289c017d18866198
Kayyro <robin.estebe8@epitech.eu>
2026-03-26 16:48:12 +0100

    ajout de la base du login.js avec fetch POST

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -3,3 +3,21 @@ const inputEmail = document.querySelector("#email");
 const inputPassword = document.querySelector("#password");
 const errorMessage = document.querySelector("#error-message");
 
+btnLogin.addEventListener("click", async () => {
+
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    });
+
+    console.log(response);
+});
